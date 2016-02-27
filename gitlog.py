0cc96e5e5df5e7910779cfed9a7b216ccea33653
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-02-27 15:51:52 +0900

    update gitignore

--- gitlog.py
+++ gitlog.py
@@ -4,11 +4,11 @@
 import subprocess, json, re, functools
 
 def _fetch_partial_commits():
-    dump =subprocess.check_output( ['git', 'log', '--pretty=format:{%n  \"commit\": \"%H\",%n  \"author\": \"%an <%ae>\",%n  \"date\": \"%ad\",%n  \"message\": \"%f\"%n},'] ).decode('UTF-8')
+    dump =subprocess.check_output( ['git', '--git-dir=../jubatus/.git', 'log', '--pretty=format:{%n  \"commit\": \"%H\",%n  \"author\": \"%an <%ae>\",%n  \"date\": \"%ad\",%n  \"message\": \"%f\"%n},'] ).decode('UTF-8')
     return json.loads("[" + dump[:-1] + "]")
 
 def _fetch_updated_files_per_revision():
-    dump =subprocess.check_output( ['git', '--no-pager', 'log', '--name-only', '--format=\'%H', '--pretty=format:'] ).decode('UTF-8')
+    dump =subprocess.check_output( ['git', '--git-dir=../jubatus/.git', '--no-pager', 'log', '--name-only', '--format=\'%H', '--pretty=format:'] ).decode('UTF-8')
     chunk=[]
     for x in re.split('\n\n', dump):
         chunk.append([xx for xx in re.split('\n', x) if len(xx)!=0])
